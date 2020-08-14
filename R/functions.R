77773e0da95490fab1106ff116a0368e6373cf0d
assignUser <github@krushak.me>
2020-08-14 11:18:54 +0200

    .hidden functions

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+#' An exported function
+#' @param x numeric
+#' @return x incremented by 1
+#' @exported
+exported <- function(x){
+    x + 1
+}
+
+
+
+.internal()
---
+++ R/internal.R
@@ -0,0 +1,7 @@
+#' An internal function
+#' @param x numeric
+#' @return x decremented by 1
+#' @noRd
+.internal <- function(x){
+    x - 1
+}
